bb010991cbc2a1a8948d976685444cfa81cfd921
theandrew168 <andrew@shallowbrooksoftware.com>
2025-09-13 17:24:26 -0500

    Close Redis connection at shutdown

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,23 +1,38 @@
-import type { Handle } from "@sveltejs/kit";
+import type { Handle, ServerInit } from "@sveltejs/kit";
+import { building } from "$app/environment";
 import { Octokit } from "octokit";
-import { createClient } from "redis";
 
 import { MemoryCachedRepoAnalyzer, RedisCachedRepoAnalyzer } from "$lib/server/analyze/cache";
 import { RedisConnection } from "$lib/server/redis";
 import { MemoryCachedRepoSearcher, RedisCachedRepoSearcher } from "$lib/server/search/cache";
 
-export const handle: Handle = async ({ event, resolve }) => {
-	let redisConnection: RedisConnection | undefined;
+let redisConnection: RedisConnection | undefined;
+
+// Perform any necessary initialization at application startup.
+export const init: ServerInit = async () => {
+	// Avoid running initialization steps during the build process.
+	// https://svelte.dev/docs/kit/building-your-app#During-the-build
+	if (building) {
+		return;
+	}
 
 	const redisConnectionURL = process.env["LOCOST_REDIS_URL"];
 	if (redisConnectionURL) {
-		const redisClient = await createClient({ url: redisConnectionURL })
-			.on("error", (err) => console.log("Redis Client Error", err))
-			.connect();
-		redisConnection = new RedisConnection(redisClient);
+		redisConnection = await RedisConnection.getInstance(redisConnectionURL);
 	}
+};
 
+// Be sure to close the redis connection when the server shuts down.
+// Otherwise, the NodeJS process will hang indefinitely.
+process.on("sveltekit:shutdown", async () => {
+	if (redisConnection) {
+		await redisConnection.close();
+	}
+});
+
+export const handle: Handle = async ({ event, resolve }) => {
 	const octokit = new Octokit();
+
 	event.locals.repoSearcher = redisConnection
 		? new RedisCachedRepoSearcher(octokit, redisConnection)
 		: new MemoryCachedRepoSearcher(octokit);
--- src/lib/server/redis/index.ts
+++ src/lib/server/redis/index.ts
@@ -1,11 +1,32 @@
-import type { createClient } from "redis";
+import { createClient } from "redis";
 
 export type RedisClient = ReturnType<typeof createClient>;
 
 export class RedisConnection {
+	private static _instance?: RedisConnection;
 	readonly client: RedisClient;
 
 	constructor(client: RedisClient) {
 		this.client = client;
+	}
+
+	static async getInstance(redisConnectionURL: string): Promise<RedisConnection> {
+		if (!this._instance) {
+			const redisClient = await createClient({ url: redisConnectionURL })
+				.on("error", (err) => console.log("Redis Client Error", err))
+				.connect();
+			this._instance = new RedisConnection(redisClient);
+		}
+
+		return this._instance;
+	}
+
+	async close(): Promise<void> {
+		if (!this.client) {
+			return;
+		}
+
+		this.client.destroy();
+		RedisConnection._instance = undefined;
 	}
 }
